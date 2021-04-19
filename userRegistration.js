ee793f85363aaaf55667cbdda25523e35a9d32ef
surendra-chouhan <chouhansurendra028@gmail.com>
2021-04-19 20:25:35 +0530

    Added logic to validate Password by having atleast 8 characters

--- userRegistration.js
+++ userRegistration.js
@@ -3,6 +3,7 @@ var read = require('readline-sync');
 const REGEX_FOR_NAME = new RegExp("[A-Z]{1}[a-z]{2,}");
 const REGEX_FOR_EMAIL = new RegExp("^[a-zA-Z0-9]+(?:[\\+._-][a-zA-Z0-9]+)*@[a-zA-Z0-9-]{1,}(?:\\.[a-zA-Z]{2,5})?(?:\\.[a-zA-Z]{2,5})?$");
 const REGEX_FOR_PHONENUMBER = new RegExp("^[0-9]{2}[ ][0-9]{10}$");
+const REGEX_FOR_PASSWORD = new RegExp(".{8,}");
 
 var firstName = read.question("Enter your First Name : ");
 if(REGEX_FOR_NAME.test(firstName)){
@@ -34,4 +35,12 @@ if(REGEX_FOR_PHONENUMBER.test(phoneNumber)){
 }
 else{
     console.log("Invalid Phone Number");
+}
+
+var password = read.question("Enter your Password : ");
+if(REGEX_FOR_PASSWORD.test(password)){
+    console.log("Password added successfully");
+}
+else{
+    console.log("Invalid Password");
 }
