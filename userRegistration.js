5132f5afb5d99dad95919ae96922653cb0771b8a
surendra-chouhan <chouhansurendra028@gmail.com>
2021-04-19 20:33:58 +0530

    Added logic to validate Password by having atleast 1 UpperCase

--- userRegistration.js
+++ userRegistration.js
@@ -3,7 +3,7 @@ var read = require('readline-sync');
 const REGEX_FOR_NAME = new RegExp("[A-Z]{1}[a-z]{2,}");
 const REGEX_FOR_EMAIL = new RegExp("^[a-zA-Z0-9]+(?:[\\+._-][a-zA-Z0-9]+)*@[a-zA-Z0-9-]{1,}(?:\\.[a-zA-Z]{2,5})?(?:\\.[a-zA-Z]{2,5})?$");
 const REGEX_FOR_PHONENUMBER = new RegExp("^[0-9]{2}[ ][0-9]{10}$");
-const REGEX_FOR_PASSWORD = new RegExp(".{8,}");
+const REGEX_FOR_PASSWORD = new RegExp("^(?=.*[A-Z]).{8,20}$");
 
 var firstName = read.question("Enter your First Name : ");
 if(REGEX_FOR_NAME.test(firstName)){
