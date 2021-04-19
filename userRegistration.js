5fd5038043a50ec13cdec9d38e7a597a0570e7a8
surendra-chouhan <chouhansurendra028@gmail.com>
2021-04-19 19:53:16 +0530

    Added logic to validate firstName entered by user

--- userRegistration.js
+++ userRegistration.js
@@ -1,1 +1,11 @@
-console.log("Welcome to User Registration Problem");
+var read = require('readline-sync');
+
+const REGEX_FOR_NAME = new RegExp("[A-Z]{1}[a-z]{2,}");
+var firstName = read.question("Enter your First Name : ");
+
+if(REGEX_FOR_NAME.test(firstName)){
+    console.log("First Name entered successfully");
+}
+else{
+    console.log("Invalid First Name");
+}
