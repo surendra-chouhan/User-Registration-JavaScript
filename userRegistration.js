3d3b835dcb0914a75de25d625098811c0ebfb857
surendra-chouhan <chouhansurendra028@gmail.com>
2021-04-19 20:10:19 +0530

    Added logic to validate Email entered by the user

--- userRegistration.js
+++ userRegistration.js
@@ -1,8 +1,9 @@
 var read = require('readline-sync');
 
 const REGEX_FOR_NAME = new RegExp("[A-Z]{1}[a-z]{2,}");
-var firstName = read.question("Enter your First Name : ");
+const REGEX_FOR_EMAIL = new RegExp("^[a-zA-Z0-9]+(?:[\\+._-][a-zA-Z0-9]+)*@[a-zA-Z0-9-]{1,}(?:\\.[a-zA-Z]{2,5})?(?:\\.[a-zA-Z]{2,5})?$");
 
+var firstName = read.question("Enter your First Name : ");
 if(REGEX_FOR_NAME.test(firstName)){
     console.log("First Name entered successfully");
 }
@@ -11,10 +12,17 @@ else{
 }
 
 var lastName = read.question("Enter your Last Name : ");
-
 if(REGEX_FOR_NAME.test(lastName)){
     console.log("Last Name entered successfully");
 }
 else{
     console.log("Invalid Last Name");
+}
+
+var email = read.question("Enter your Email ID : ");
+if(REGEX_FOR_EMAIL.test(email)){
+    console.log("Email added successfully");
+}
+else{
+    console.log("Invalid Email")
 }
