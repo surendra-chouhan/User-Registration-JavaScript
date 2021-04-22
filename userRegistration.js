628a37ee6cbdc6e1157869e8b55c8ae10e7cf46b
surendra-chouhan <chouhansurendra028@gmail.com>
2021-04-22 17:25:44 +0530

    Refactored the code to use functions

--- userRegistration.js
+++ userRegistration.js
@@ -5,42 +5,63 @@ const REGEX_FOR_EMAIL = new RegExp("^[a-zA-Z0-9]+(?:[\\+._-][a-zA-Z0-9]+)*@[a-zA
 const REGEX_FOR_PHONENUMBER = new RegExp("^[0-9]{2}[ ][0-9]{10}$");
 const REGEX_FOR_PASSWORD = new RegExp("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=[^$@!#%*?&]*[$#@!%*?&][^$@!#%*?&]*$).{8,}");
 
-var firstName = read.question("Enter your First Name : ");
-if(REGEX_FOR_NAME.test(firstName)){
-    console.log("First Name entered successfully");
-}
-else{
-    console.log("Invalid First Name");
-}
-
-var lastName = read.question("Enter your Last Name : ");
-if(REGEX_FOR_NAME.test(lastName)){
-    console.log("Last Name entered successfully");
-}
-else{
-    console.log("Invalid Last Name");
+function validateFirstName(){
+    var firstName = read.question("Enter your First Name : ");
+    if(REGEX_FOR_NAME.test(firstName)){
+        console.log("First Name entered successfully");
+    }
+    else{
+        console.log("Invalid First Name. Please Enter Again");
+        validateFirstName();
+    }
 }
 
-var email = read.question("Enter your Email ID : ");
-if(REGEX_FOR_EMAIL.test(email)){
-    console.log("Email added successfully");
-}
-else{
-    console.log("Invalid Email")
-}
-
-var phoneNumber = read.question("Enter your Phone Number : ");
-if(REGEX_FOR_PHONENUMBER.test(phoneNumber)){
-    console.log("Phone Number added successfully");
-}
-else{
-    console.log("Invalid Phone Number");
+function validateLastName(){
+    var lastName = read.question("Enter your Last Name : ");
+    if(REGEX_FOR_NAME.test(lastName)){
+        console.log("Last Name entered successfully");
+    }
+    else{
+        console.log("Invalid Last Name. Please Enter Again");
+        validateLastName();
+    }
 }
 
-var password = read.question("Enter your Password : ");
-if(REGEX_FOR_PASSWORD.test(password)){
-    console.log("Password added successfully");
+function validateEmail(){ 
+    var email = read.question("Enter your Email ID : ");
+    if(REGEX_FOR_EMAIL.test(email)){
+        console.log("Email added successfully");    
+    }
+    else{
+        console.log("Invalid Email. Please Enter Again")
+        validateEmail();
+    }
 }
-else{
-    console.log("Invalid Password");
+
+function validatePhoneNumber(){
+    var phoneNumber = read.question("Enter your Phone Number : ");
+    if(REGEX_FOR_PHONENUMBER.test(phoneNumber)){
+        console.log("Phone Number added successfully");
+    }
+    else{
+        console.log("Invalid Phone Number. Please Enter Again");
+        validatePhoneNumber();
+    }
 }
+
+function validatePassword(){
+    var password = read.question("Enter your Password : ");
+    if(REGEX_FOR_PASSWORD.test(password)){
+        console.log("Password added successfully");
+    }
+    else{
+        console.log("Invalid Password. Please Enter Again");
+        validatePassword();
+    }
+}
+
+validateFirstName();
+validateLastName();
+validateEmail();
+validatePhoneNumber();
+validatePassword();
